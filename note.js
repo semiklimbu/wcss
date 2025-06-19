baa48098aaaee3f979e785db06e1f9973178ff7f
semiklimbu <srv.study.@gamil.com>
2025-06-19 08:23:24 +0545

    update7

---
+++ note.js
@@ -0,0 +1,23 @@
+
+
+
+
+// alert("good morning")
+// console.log("hello underwater city")
+// const name= "magma village"
+// console.log(
+//     name
+// )
+
+
+
+
+
+
+
+alert("goodmorning")
+// 
+
+let name=prompt("bro enter your username")
+console.log(name)
+alert("hey where are you going " + name)
